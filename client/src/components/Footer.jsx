d0ad16851ac10f33759c88095fd632567c06771b
leo-r37 <leandro_rocha93@hotmail.com>
2023-01-11 01:34:51 -0300

    added links in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,26 @@
 import s from "./Footer.module.css";
+import logoLinkedIn from "../imgs/linkedin.png";
+import logoGitHub from "../imgs/github.png";
 
 const Footer = () => {
   return (
     <div className={s.container}>
       DogPaw - Dog's breeds database - Made by Leandro Rocha
+      <a
+        href="https://www.linkedin.com/in/leandro-ezequiel-rocha/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={logoLinkedIn} className={s.logo} alt="Logo Linkedin" />
+      </a>
+
+      <a
+        href="https://github.com/leo-r37"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={logoGitHub} className={s.logo} alt="Logo GitHub" />
+      </a>
     </div>
   );
 };
