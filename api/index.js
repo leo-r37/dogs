2e8e8a0164bc8d93b62ea135a0344ef712724b8f
leo-r37 <leandro_rocha93@hotmail.com>
2023-01-12 12:55:22 -0300

    deploying

--- api/index.js
+++ api/index.js
@@ -19,13 +19,15 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+require('dotenv').config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ 
   // force: true, 
   // alter: true 
 }).then(() => {
-  server.listen(3001, () => {
-    console.log("Server listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("Server listening at ", PORT); // eslint-disable-line no-console
   });
 });
