cda73ceb90f1f1b72cdc5b7f7611ac06c04851fb
leo-r37 <leandro_rocha93@hotmail.com>
2023-03-19 18:34:23 -0300

    modify front_url in back server

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,7 +4,7 @@ import rootReducer from "./reducers/index";
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunkMiddleware)
 );
 
